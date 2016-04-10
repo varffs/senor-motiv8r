c6ec7e3c47c7b98debb1f039db5f80d1dc729235
Patrick Best <patrickbest@patrickbest.com>
2016-04-10 00:39:59 -0500

    version 0.1.0

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 # Señor Motiv8r
 
-Motivate yourself to work on personal projects by using a structured Trello board and recieve motivation to work thru that board via Señor Motiv8r.
+Motivate yourself to work on personal projects by using a structured Trello board and receive motivation to work thru tasks on that board via Señor Motiv8r.
 
 ### v0.1.0
 
-Get motivation by receiving a schedules push notification (sent with ITFFF via Maker channel) with a random project and task you could work on if you feel like.
+Get motivation by receiving a scheduled push notification (sent with ITFFF via Maker channel) with a random project and task you could work on if you feel like.
 
 For example get a notification every Sunday at 10:00 suggesting a possible task to progress your personal projects.
 
-Requires a Trello board called `Personal Projects` that matches a structure. One main list called `Projects` with cards for each project, then lists with the name matching the card in `Projects` with cards for tasks for that project. For example a project card called `Make chair` with a list called `Make chair` and cards in that list for sketching, drawing plans, buying materials, construction, painting, etc.
+Requires a Trello board specified by ID with a structure: One main list called `Projects` with cards for each project, then lists with the name matching the card in `Projects` with cards for tasks for that project. For example a project card called `Make chair` with a list called `Make chair` and cards in that list for sketching, drawing plans, buying materials, construction, painting, etc.
 
 Perhaps this board and main list can be specified via Trello IDs later in this project and optionally set as configuration
 
 ### How to run
 
-- clone the repo and set values for the `.env.example` file and to rename `.env`
+- setup the ITFFF Maker channel and create a recipe and get the webhook url
+- clone the repo and set values for the `.env.example` file and rename to `.env`
 - `npm install`
 - `node senor-motiv8r.js` (or use PM2 or similar to run the node app)
 
 ### TODO
 
-- setup packages
-- create object with init function
-- init checks for DEBUG mode if not sets up a cron
-- function gets the data from Trello. The active projects and chooses 1 randomly and gets a random card from its list
-- function sends the POST to the webhook url
+- test @ v0.1.0
+
+#### The future
+
+I'm waiting for the Facebook Messenger chat SDK to be released and then this will likely become a chatbot that can edit the Trello board and suggest projects to work on in an interactive way.
---
+++ senor-motiv8r.js
@@ -0,0 +1,205 @@
+require('dotenv').config();
+
+var Promise = require('promise');
+var _ = require('lodash');
+var request = require('request');
+
+var CronJob = require('cron').CronJob;
+var randomNumber = require('random-number');
+var Trello = require('node-trello');
+
+/*
+if (process.env.DEBUG === 'true') {
+  console.log(process.env);
+}
+*/
+
+// SETUP TRELLO ACCESS
+
+var trelloKey = process.env.TRELLO_KEY;
+var trelloToken = process.env.TRELLO_TOKEN;
+
+var trello = new Trello(trelloKey, trelloToken);
+
+// MR MOTIVATOR
+
+var Motiv8r = {
+  mainBoard: process.env.TRELLO_BOARD_ID,
+  webhook: process.env.IFTTT_WEBHOOK_URL,
+  timezone: process.env.TIMEZONE,
+  schedule: process.env.CRON_TIME,
+
+  init: function() {
+    var _this = this;
+
+    if (process.env.DEBUG !== 'true') {
+
+      var job = new CronJob({
+        cronTime: _this.schedule,
+        onTick: function() {
+
+          _this.sendMotivation();
+
+        },
+        start: true,
+        timeZone: _this.timezone
+      });
+
+      job.start();
+
+    } else {
+
+      _this.sendMotivation();
+
+    }
+
+  },
+
+  sendMotivation: function() {
+    var _this = this;
+
+    Promise.all([_this.getTrelloLists()]).then(function(res) {
+      Promise.all([_this.getTrelloProjects()]).then(function(res) {
+
+        _this.chooseProject();
+
+        Promise.all([_this.getProjectTasks()]).then(function(res) {
+          _this.chooseTask();
+          _this.sendNotification(_this.generateMessage());
+        });
+
+      });
+
+    });
+
+  },
+
+  getTrelloLists: function() {
+    var _this = this;
+
+    return new Promise(function(resolve, reject) {
+
+      var requestUrl = '/1/boards/' + _this.mainBoard + '/lists';
+
+      trello.get(requestUrl, function(err, res) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          _this.trelloLists = res;
+          resolve(res);
+        }
+
+      });
+
+    });
+  },
+
+  getTrelloProjects: function() {
+    var _this = this;
+
+    return new Promise(function(resolve, reject) {
+
+      var projectsList = _this.trelloLists[_.findIndex(_this.trelloLists, {name: 'Projects'})];
+      var requestUrl = '/1/lists/' + projectsList.id;
+
+      trello.get(requestUrl, {cards: 'open'}, function(err, res) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          _this.trelloProjects = res.cards;
+          resolve(res);
+        }
+
+      });
+
+    });
+
+  },
+
+  chooseProject: function() {
+    var _this = this;
+    var getRandom = randomNumber.generator({
+      min: 0,
+      max: _this.trelloProjects.length - 1,
+      integer: true,
+    });
+
+    _this.chosenProject = _this.trelloProjects[getRandom()];
+
+    return _this.chosenProject;
+
+  },
+
+  getProjectTasks: function() {
+    var _this = this;
+
+    return new Promise(function(resolve, reject) {
+
+      var chosenProjectCard = _this.trelloLists[_.findIndex(_this.trelloLists, {name: _this.chosenProject.name})];;
+      var requestUrl = '/1/lists/' + chosenProjectCard.id;
+
+      trello.get(requestUrl, {cards: 'open'}, function(err, res) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          _this.projectTasks = res.cards;
+          resolve(res);
+        }
+
+      });
+
+    });
+
+  },
+
+  chooseTask: function() {
+    var _this = this;
+    var getRandom = randomNumber.generator({
+      min: 0,
+      max: _this.projectTasks.length - 1,
+      integer: true,
+    });
+
+    _this.chosenTask = _this.projectTasks[getRandom()];
+
+    return _this.chosenTask;
+
+  },
+
+  generateMessage: function() {
+    var _this = this;
+
+    return 'Why not work on ' + _this.chosenProject.name + '? You could do ' + _this.chosenTask.name + ' :)';
+
+  },
+
+  sendNotification: function(message) {
+    var _this = this;
+
+    if (process.env.DEBUG === 'true') {
+      console.log(message);
+    }
+
+    var options = {
+      url: _this.webhook,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: '{"value1":"' + message + '"}'
+    };
+
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    });
+
+  },
+
+};
+
+Motiv8r.init();
